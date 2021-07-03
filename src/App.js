8e841f6916fa1021cbef63431d0ecae0cc49f7d5
Reginaldo Simon Bonaldi <reginaldobonaldi22@gmail.com>
2021-07-03 15:59:32 -0300

    retornando array de elementos

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
 class App extends Component {
   state = {
-    name: "Reginaldo",
-    conter: 0
+    posts: [
+      {
+        id: 1,
+        title: "Título 1",
+        body: "Corpo 1"
+      },
+      {
+        id: 2,
+        title: "Título 2",
+        body: "Corpo 2"
+      },
+      {
+        id: 3,
+        title: "Título 2",
+        body: "Corpo 2"
+      },
+    ]
   }
 
-  updateConter = () => {
-    const { conter } = this.state;
-
-    this.setState({ conter: conter + 1 });
-  }
-
-  changeName = (event) => {
-    event.preventDefault();
-    this.setState({ name: "Reginaldo Bonaldi" });
-
-    this.updateConter();
-  }
   render() {
 
-    const { name, conter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {name} {conter}
-          </p>
-          <a onClick={this.changeName}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Click here
-          </a>
-        </header>
+        <div>
+          {posts.map((post) => {
+            return (
+              <div key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+            )
+          }
+          )}
+        </div>
+        <div>
+        </div>
       </div>
     );
   }
